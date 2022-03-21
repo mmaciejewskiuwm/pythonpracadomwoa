8a4a5cdd84e8e77793280d70c98f1ee9e18d8d66
mikur <100271723+mmaciejewskiuwm@users.noreply.github.com>
2022-03-21 21:44:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+# Zad 1.
+
+# lista = ['siata', 'pila', 'kosz']
+# print(lista)
+# lista.reverse()
+# print(lista)
+# lista.append('pingpong')
+# lista.append('plywanie)
+# print(lista)
+
+# Zad 2.
+
+# slownik = {'siemandero':'ulepszone siema',"kys":'zyczenie dlugiego zycia', 'kc':'w tlumaczeniu kocham cie' }
+# print(slownik['siemandero'])
+# print(slownik['wtf'])
+# print(slownik['lol'])
+
+# Zad 3.
+
+# slownik = {'eu4':'Europa universalis 4',"lol":'league of legends', 'h5':'heroes of might and magic 5' }
+# print(len(dict.keys(slownik)))
+
+# Zad 4.
+
+# napis = input("Wpisz se cos: ")
+# print(napis.count('a'))
+
+# Zad 5.
+
+# import sys as system
+# system.stdout.write("liczby 3 poproszem  ")
+# a = system.stdin.readline()
+# a=int(a)
+# b = system.stdin.readline()
+# b=int(b)
+# c = system.stdin.readline()
+# c=int(c)
+# d = a*b + c
+# d = str(d)
+# system.stdout.write(d)
+
+# Zad 6.
+
+# a = input('daj 3 liczby')
+# b = input()
+# c = input()
+# a=int(a)
+# b=int(b)
+# c=int(c)
+# print(max(a, b, c ))
+
+# Zad 7.
+
+# lista = [ 9, 43, 86, 5.25, 2.78, 9.99, 1.23]
+# for x in lista:
+#     print(type(x))
+# for x in lista:
+#     print(x*x)
+
+# Zad 8.
+
+# a=0
+# lista = []
+# while(a<10):
+#     b=int(input("daj liczbe: "))
+#     if(b%2==0):
+#         lista.append(b)
+#
+#     a+=1
+# print(lista)
+
+# Zad. 9.
+#
+# import math
+# a = int(input('Wpisz liczbe: '))
+# if(a<0):
+#     print('innom liczbe daj')
+# else:
+#     print(math.sqrt(a))
